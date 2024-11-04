65339ab8d2dad304e2168f8edb9beb162594ea1b
Daan Verbeek <daanverbeek13@gmail.com>
2024-11-04 14:44:16 +0100

    song overview done. design not included

--- Beatport_UI/Controllers/SongController.cs
+++ Beatport_UI/Controllers/SongController.cs
@@ -1,14 +1,38 @@
 using Beatport_UI.Models;
+using dotenv.net;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
 
 namespace Beatport_UI.Controllers;
 
 public class SongController : Controller
 {
+    
+    private readonly string connectionStr = DotEnv.Read()["DEFAULT_CONNECTION"];
     // GET
     public IActionResult Index()
     {
-        return View();
+        List<SongModel> songs = new List<SongModel>();
+        using (MySqlConnection mySqlConnection = new MySqlConnection(connectionStr))
+        {
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM songs", mySqlConnection);
+            
+            mySqlConnection.Open();
+            
+            MySqlDataReader reader = cmd.ExecuteReader();
+            
+            while (reader.Read())
+            {
+                songs.Add(new SongModel
+                {
+                    Id = reader.GetInt32("id"),
+                    Title = reader.GetString("title"),
+                    Genre = reader.GetString("genre"),
+                    Bpm = reader.GetInt32("bpm")
+                });
+            }
+        }
+        return View(songs);
     }
     
     
@@ -20,9 +44,6 @@ public class SongController : Controller
     [HttpPost] 
     public ActionResult CreateSong(SongModel sm)
     {
-        Console.WriteLine(sm.Title);
-        Console.WriteLine(sm.Bpm);
-        Console.WriteLine(sm.Genre);
  
         return View("Index");
     }
