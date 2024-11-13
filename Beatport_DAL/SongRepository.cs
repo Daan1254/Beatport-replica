9b1a62022bcbe8711ca3857ac72b0b2d1a9a315d
Daan Verbeek <daanverbeek13@gmail.com>
2024-11-13 11:25:41 +0100

    Added error handling

--- Beatport_DAL/SongRepository.cs
+++ Beatport_DAL/SongRepository.cs
@@ -16,109 +16,141 @@ public class SongRepository : ISongRepository
     
     public List<SongDto> GetAllSongs()
     {
-        List<SongDto> songs = new List<SongDto>();
-        using (MySqlConnection mySqlConnection = new MySqlConnection(connectionStr))
+        try
         {
-            MySqlCommand cmd = new MySqlCommand("SELECT * FROM songs", mySqlConnection);
-            
-            mySqlConnection.Open();
-            
-            MySqlDataReader reader = cmd.ExecuteReader();
-            
-            while (reader.Read())
+            List<SongDto> songs = new List<SongDto>();
+            using (MySqlConnection mySqlConnection = new MySqlConnection(connectionStr))
             {
-                songs.Add(new SongDto
-                {
-                    Id = reader.GetInt32("id"),
-                    Title = reader.GetString("title"),
-                    Genre = reader.GetString("genre"),
-                    Bpm = reader.GetInt32("bpm"),
-                    CreatedAt = reader.GetDateTime("CreatedAt"),
-                    UpdatedAt = reader.IsDBNull(reader.GetOrdinal("UpdatedAt")) ? (DateTime?)null : reader.GetDateTime("UpdatedAt"),
-                    DeletedAt = reader.IsDBNull(reader.GetOrdinal("DeletedAt")) ? (DateTime?)null : reader.GetDateTime("DeletedAt"),
-                });
-            }
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM songs", mySqlConnection);
             
-            mySqlConnection.Close();
+                mySqlConnection.Open();
+            
+                MySqlDataReader reader = cmd.ExecuteReader();
+            
+                while (reader.Read())
+                {
+                    songs.Add(new SongDto
+                    {
+                        Id = reader.GetInt32("id"),
+                        Title = reader.GetString("title"),
+                        Genre = reader.GetString("genre"),
+                        Bpm = reader.GetInt32("bpm"),
+                        CreatedAt = reader.GetDateTime("CreatedAt"),
+                        UpdatedAt = reader.IsDBNull(reader.GetOrdinal("UpdatedAt")) ? (DateTime?)null : reader.GetDateTime("UpdatedAt"),
+                        DeletedAt = reader.IsDBNull(reader.GetOrdinal("DeletedAt")) ? (DateTime?)null : reader.GetDateTime("DeletedAt"),
+                    });
+                }
+            
+                mySqlConnection.Close();
+            }
+            return songs;
+        } catch (Exception e)
+        {
+            throw new Exception("An error occurred while fetching songs.", e);
         }
-        return songs;
     }
     
     public SongDto? GetSong(int id)
     {
-        SongDto? song = null;
-        using (MySqlConnection mySqlConnection = new MySqlConnection(connectionStr))
+        try
         {
-            MySqlCommand cmd = new MySqlCommand("SELECT * FROM songs WHERE id = @id", mySqlConnection);
-            cmd.Parameters.AddWithValue("@id", id);
-            
-            mySqlConnection.Open();
-            
-            MySqlDataReader reader = cmd.ExecuteReader();
-            
-            if (reader.Read())
+            SongDto? song = null;
+            using (MySqlConnection mySqlConnection = new MySqlConnection(connectionStr))
             {
-                song = new SongDto
-                {
-                    Id = reader.GetInt32("id"),
-                    Title = reader.GetString("title"),
-                    Genre = reader.GetString("genre"),
-                    Bpm = reader.GetInt32("bpm"),
-                    CreatedAt = reader.GetDateTime("CreatedAt"),
-                    UpdatedAt = reader.IsDBNull(reader.GetOrdinal("UpdatedAt")) ? (DateTime?)null : reader.GetDateTime("UpdatedAt"),
-                    DeletedAt = reader.IsDBNull(reader.GetOrdinal("DeletedAt")) ? (DateTime?)null : reader.GetDateTime("DeletedAt"),
-                };
-            }
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM songs WHERE id = @id", mySqlConnection);
+                cmd.Parameters.AddWithValue("@id", id);
             
-            mySqlConnection.Close();
+                mySqlConnection.Open();
+            
+                MySqlDataReader reader = cmd.ExecuteReader();
+            
+                if (reader.Read())
+                {
+                    song = new SongDto
+                    {
+                        Id = reader.GetInt32("id"),
+                        Title = reader.GetString("title"),
+                        Genre = reader.GetString("genre"),
+                        Bpm = reader.GetInt32("bpm"),
+                        CreatedAt = reader.GetDateTime("CreatedAt"),
+                        UpdatedAt = reader.IsDBNull(reader.GetOrdinal("UpdatedAt")) ? (DateTime?)null : reader.GetDateTime("UpdatedAt"),
+                        DeletedAt = reader.IsDBNull(reader.GetOrdinal("DeletedAt")) ? (DateTime?)null : reader.GetDateTime("DeletedAt"),
+                    };
+                }
+            
+                mySqlConnection.Close();
+            }
+            return song;  
+        } catch (Exception e)
+        {
+            throw new Exception("An error occurred while fetching song.", e);
         }
-        return song;
     }
 
     public bool CreateSong(CreateEditSongDto createEditSongDto)
     {
-        using (MySqlConnection mySqlConnection = new MySqlConnection(connectionStr))
+        try
         {
-            MySqlCommand cmd = new MySqlCommand("INSERT INTO songs (title, genre, bpm) VALUES (@title, @genre, @bpm)",
-                mySqlConnection);
-            cmd.Parameters.AddWithValue("@title", createEditSongDto.Title);
-            cmd.Parameters.AddWithValue("@genre", createEditSongDto.Genre);
-            cmd.Parameters.AddWithValue("@bpm", createEditSongDto.Bpm);
+            using (MySqlConnection mySqlConnection = new MySqlConnection(connectionStr))
+            {
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO songs (title, genre, bpm) VALUES (@title, @genre, @bpm)",
+                    mySqlConnection);
+                cmd.Parameters.AddWithValue("@title", createEditSongDto.Title);
+                cmd.Parameters.AddWithValue("@genre", createEditSongDto.Genre);
+                cmd.Parameters.AddWithValue("@bpm", createEditSongDto.Bpm);
 
-            mySqlConnection.Open();
+                mySqlConnection.Open();
 
-            return cmd.ExecuteNonQuery() > 0;
+                return cmd.ExecuteNonQuery() > 0;
+            }
         }
+        catch (Exception e)
+        {
+            throw new Exception("An error occurred while creating song.", e);
+        }
+        
     }
     
     
     public bool EditSong(int id, CreateEditSongDto createEditSongDto)
     {
-        using (MySqlConnection mySqlConnection = new MySqlConnection(connectionStr))
+        try
         {
-            MySqlCommand cmd = new MySqlCommand("UPDATE songs SET title = @title, genre = @genre, bpm = @bpm WHERE id = @id",
-                mySqlConnection);
-            cmd.Parameters.AddWithValue("@title", createEditSongDto.Title);
-            cmd.Parameters.AddWithValue("@genre", createEditSongDto.Genre);
-            cmd.Parameters.AddWithValue("@bpm", createEditSongDto.Bpm);
-            cmd.Parameters.AddWithValue("@id", id);
+            using (MySqlConnection mySqlConnection = new MySqlConnection(connectionStr))
+            {
+                MySqlCommand cmd = new MySqlCommand("UPDATE songs SET title = @title, genre = @genre, bpm = @bpm WHERE id = @id",
+                    mySqlConnection);
+                cmd.Parameters.AddWithValue("@title", createEditSongDto.Title);
+                cmd.Parameters.AddWithValue("@genre", createEditSongDto.Genre);
+                cmd.Parameters.AddWithValue("@bpm", createEditSongDto.Bpm);
+                cmd.Parameters.AddWithValue("@id", id);
 
-            mySqlConnection.Open();
+                mySqlConnection.Open();
 
-            return cmd.ExecuteNonQuery() > 0;
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        } catch (Exception e)
+        {
+            throw new Exception("An error occurred while editing song.", e);
         }
     }
 
     public bool DeleteSong(int id)
     {
-        using (MySqlConnection mySqlConnection = new MySqlConnection(connectionStr))
+        try
         {
-            MySqlCommand cmd = new MySqlCommand("DELETE FROM songs WHERE id = @id", mySqlConnection);
-            cmd.Parameters.AddWithValue("@id", id);
+            using (MySqlConnection mySqlConnection = new MySqlConnection(connectionStr))
+            {
+                MySqlCommand cmd = new MySqlCommand("DELETE FROM songs WHERE id = @id", mySqlConnection);
+                cmd.Parameters.AddWithValue("@id", id);
 
-            mySqlConnection.Open();
+                mySqlConnection.Open();
 
-            return cmd.ExecuteNonQuery() > 0;
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        } catch (Exception e)
+        {
+            throw new Exception("An error occurred while deleting song.", e);
         }
     }
 }
